b507fb40f481cdbc4e2ed35c6ee97df1e2605d11
시골프로그래머 <civilian7@gmail.com>
2025-07-23 16:58:04 +0900

    Add files via upload

---
+++ 01.RESTServer/src/03.Database/Controller.Group.pas
@@ -0,0 +1,70 @@
+﻿unit Controller.Group;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.JSON,
+  Data.DB,
+  uDBPool,
+  Horse;
+
+implementation
+
+procedure RegisterController;
+begin
+  THorse.Get('/group',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text :=
+        'SELECT ' +
+        '  USR_ID ' +
+        '  , USR_NM ' +
+        '  , USR_LVL ' +
+        'FROM ' +
+        '  USRS ';
+        LQuery.Open;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+
+  THorse.Get('/group/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text := '''
+        SELECT
+          USR_ID
+          , USR_NM
+          , USR_LVL
+        FROM
+          USRS
+        WHERE
+          USR_ID = :USR_ID
+        ''';
+        LQuery.Params.ParamByName('USR_ID').AsString := Req.Params['id'];
+        LQuery.Open;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+end;
+
+initialization
+  RegisterController;
+end.
---
+++ 01.RESTServer/src/03.Database/Controller.User.pas
@@ -0,0 +1,170 @@
+﻿unit Controller.User;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.JSON,
+  Data.DB,
+  uDBPool,
+  Horse;
+
+implementation
+
+procedure RegisterController;
+begin
+  {$REGION '전체 조회'}
+  THorse.Get('/user',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text :=
+        'SELECT ' +
+        '  USR_ID ' +
+        '  , USR_NM ' +
+        '  , USR_LVL ' +
+        'FROM ' +
+        '  USRS ';
+        LQuery.Open;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+  {$ENDREGION}
+
+  {$REGION '단건 조회'}
+  THorse.Get('/user/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text := '''
+        SELECT
+          USR_ID
+          , USR_NM
+          , USR_LVL
+        FROM
+          USRS
+        WHERE
+          USR_ID = :USR_ID
+        ''';
+        LQuery.Params.ParamByName('USR_ID').AsString := Req.Params['id'];
+        LQuery.Open;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+  {$ENDREGION}
+
+  // 삭제
+  THorse.Delete('/user/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text := '''
+        DELETE
+        FROM
+          USRS
+        WHERE
+          USR_ID = :USR_ID
+        ''';
+        LQuery.Params.ParamByName('USR_ID').AsString := Req.Params['id'];
+        LQuery.ExecSQL;
+
+        Res.ContentType('application/json');
+        Res.Send('{"result": "OK"}');
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+
+  // 등록
+  THorse.Post('/user',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text := '''
+        INSERT INTO USRS
+        (
+          USR_ID
+          , USR_PW
+          , USR_NM
+          , USR_LVL
+        )
+        VALUES
+        (
+          :USR_ID
+          , :USR_PW
+          , :USR_NM
+          , :USR_LVL
+        )
+        ''';
+
+        var LJSON := TJSONObject.ParseJSONValue(Req.Body) as TJSONObject;
+        LQuery.Params.ParamByName('USR_ID').AsString := LJSON.GetValue<string>('usr_id');
+        LQuery.Params.ParamByName('USR_PW').AsString := '1234';
+        LQuery.Params.ParamByName('USR_NM').AsString := LJSON.GetValue<string>('usr_nm');
+        LQuery.Params.ParamByName('USR_LVL').AsInteger := LJSON.GetValue<Integer>('usr_lvl');
+        LQuery.ExecSQL;
+        LJSON.Free;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+
+  // 수정
+  THorse.Put('/user/:id',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      var LConnection := TDatabase.GetConnection;
+      var LQuery := LConnection.GetQuery;
+      try
+        LQuery.SQL.Text := '''
+        UPDATE USRS
+        SET
+          USR_NM = :USR_NM
+          , USR_LVL = :USR_LVL
+        WHERE
+          USR_ID = :USR_ID
+        ''';
+
+        var LJSON := TJSONObject.ParseJSONValue(Req.Body) as TJSONObject;
+        LQuery.Params.ParamByName('USR_ID').AsString := Req.Params['id'];
+        LQuery.Params.ParamByName('USR_NM').AsString := LJSON.GetValue<string>('usr_nm');
+        LQuery.Params.ParamByName('USR_LVL').AsInteger := LJSON.GetValue<Integer>('usr_lvl');
+        LQuery.ExecSQL;
+        LJSON.Free;
+
+        Res.ContentType('application/json');
+        Res.Send(LQuery.ToJSON);
+      finally
+        LQuery.Free;
+        LConnection.Free;
+      end;
+    end);
+end;
+
+initialization
+  RegisterController;
+end.
